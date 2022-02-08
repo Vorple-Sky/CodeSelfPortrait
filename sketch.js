2202394dec63ef55556e6fdb1f0dc1c6b261e061
Vorple-Sky <hannahg3211@gmail.com>
2022-02-08 13:43:48 -0800

    Added files

---
+++ sketch.js
@@ -0,0 +1,111 @@
+/***********************************************************************************
+  SimpleShapes
+  by Scott Kildall
+
+  Sketch for drawing some very simple shapes
+
+***********************************************************************************/
+
+// Single line comment
+/*
+multiline comment
+*/
+
+// Global Variables
+var ellipseXPos = 0;
+var ellipseYPos = 300;
+var ellipseDist = 40;
+
+var XSpeed =4;
+var YSpeed =10;
+var xPos =100;
+var yPos =300;
+
+// Setup code goes here
+function setup() {
+  createCanvas(1000, 800);
+  console.log("Starting up Simple Shapes");
+  rectMode(CENTER);
+  ellipseMode(CENTER);
+
+  console.log("Width = " + width);
+  console.log("Height = " + height);
+
+  // set ellipse X pos
+  ellipseXPos = width/2 - 250;
+  xPos = width/2;
+  yPos = height/2;
+ }
+
+// Draw code goes here
+function draw() {
+  background(34,50,204);
+  fill(225);
+  noStroke();
+
+  drawRedEllipse(xPos,yPos,100);
+  xPos += XSpeed;
+  yPos += YSpeed;
+
+  if( xPos > width || xPos < 0){
+    XSpeed = -XSpeed;
+  }
+ 
+  if(yPos > height || yPos < 0){
+    YSpeed = -YSpeed;
+  }
+
+
+  /*for( let i = 0; i < 8; i++){
+    for( let j = 0; j < 10; j++){
+      drawRedEllipse(60 + (i*200), 60 +(j*100), 60 +(i*15));
+    }
+  }*/
+  
+}
+
+function drawRedEllipse(x,y,w) {
+  push();
+  fill(255 - w,0,0);
+  ellipse(x,y,w,60);
+  pop();
+}
+
+function drawWhiteRect(x,y) {
+  push();
+  rect(x,y,60,60);
+  pop();
+}
+
+function drawGreenCircle(x,y) {
+  push();
+  stroke(0);
+  strokeWeight(3);
+  fill(0,225,0);
+  circle(x,y,30);
+  pop();
+}
+
+function drawEllipses() {
+  // Ellipse #1
+  fill(0,255,0);
+  ellipse(ellipseXPos,ellipseYPos,200,80);
+
+  // Ellipse #2
+  fill(0,255,0);
+  ellipse(ellipseXPos,ellipseYPos+ellipseDist,200,80);
+}
+
+function drawRects() {
+ // Center Rectangle
+  fill(255,0,0);
+  stroke(220);
+  strokeWeight(3);
+  rect( width/2,height/2,200,100);
+
+  // Tall Rect
+  fill(126,50,20);
+  stroke(27);
+  strokeWeight(2);
+  rect( 100,height/2,40,400);
+ }
