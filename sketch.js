e40305fbaffc029eb12e952bc016c0693d3d5397
Vorple-Sky <hannahg3211@gmail.com>
2022-02-09 18:13:38 -0800

    added face and reference image change

--- sketch.js
+++ sketch.js
@@ -11,7 +11,7 @@ var gDebugMode = false;
 let img; // Declare variable 'img'
 
 function preload(){
-  img1 = loadImage("cat.png")
+  img1 = loadImage("Depression.png")
 }
 
 // Setup code goes here
@@ -31,16 +31,17 @@ function setup() {
 
 // Draw code goes here
 function draw() {
-  background(34,50,204);
-  image(img1,0,0);
-  fill(240, 120,0);
+  background(132,143,165);
+  image(img1,0,0, 1000, 800);
+  fill(240, 0,0);//shape color
   noStroke();
   
 
   if(gDebugMode == true ){
     drawDebugInfo();
   }
-  ellipse(width/2, height/2, 200, 100);
+  
+  drawHead();
   
 }
 
@@ -53,4 +54,25 @@ function keyTyped(){
 function drawDebugInfo(){
   fill(225);
   text("X: " + mouseX + "  Y:" + mouseY, 20, height - 20);
+}
+
+function drawHead(){
+  push();
+  stroke(0);
+  strokeWeight(0);
+  fill(231,216,201);
+  beginShape();
+  vertex(354, 253);
+  vertex(361, 392);
+  vertex(396, 459);
+  vertex(477, 500);
+  vertex(565, 459);
+  vertex(600, 392);
+  vertex(610, 253);
+  vertex(482, 210);
+  endShape();
+
+
+
+  pop();
 }
